a5465da801d8f59679e0581052cfad609ee5ec47
amitsept1976 <amitsbudhe@gmail.com>
2026-03-21 06:09:45 +0000

    seed working

--- models.py
+++ models.py
@@ -8,8 +8,8 @@ class Remedy(db.Model):
     __tablename__ = "remedies"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)          # e.g. "Calc fluor"
-    full_name = db.Column(db.String(200), nullable=False)     # e.g. "Calcium fluoride"
+    name = db.Column(db.String(255), nullable=False)          # e.g. "Calc fluor"
+    full_name = db.Column(db.String(255), nullable=False)     # e.g. "Calcium fluoride"
     description = db.Column(db.Text, nullable=True)           # what it’s used for
     keywords = db.Column(db.Text, nullable=True)              # comma/space separated tags
 
